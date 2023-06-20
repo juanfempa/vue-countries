545997f9e44d4ca207bc370036187a20a01a949f
Juan CASA <belda_juamol@gva.es>
2023-06-20 17:51:49 +0200

    Recuperados los paises

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+const urlApi = 'https://restcountries.com/v3.1/region/europe'
+
+function recuperaCountries() {
+  fetch(urlApi)
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      console.log(data)
+      countries.value = data
+    })
+}
+
+recuperaCountries();
+
+const countries = ref([])
+</script>
+
 <template>
-  App limpia
+  {{ countries }}
 </template>
