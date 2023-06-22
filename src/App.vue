ba1e10b0ca00e11229d2e4cbc704c20ccc4664bc
Juan CASA <belda_juamol@gva.es>
2023-06-22 17:28:57 +0200

    Añadidas explicaciones

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <script setup>
-import { ref } from 'vue'
-const urlApi = 'https://restcountries.com/v3.1/region/europe'
+import { ref } from 'vue';
+const urlApi = 'https://restcountries.com/v3.1/region/europe';
 
 function recuperaCountries() {
   fetch(urlApi)
     .then((respuesta) => respuesta.json())
     .then((data) => {
-      console.log(data)
-      countries.value = data
-    })
+      console.log(data);
+      countries.value = data;
+    });
 }
 
 recuperaCountries();
 
-const countries = ref([])
+const countries = ref([]);
+const isPrimary = ref(false);
+const isDanger = ref(true);
+
+const toggleButton = () => {
+  isPrimary.value = !isPrimary.value;
+  isDanger.value = !isDanger.value;
+};
 </script>
 
 <template>
-  <ol>
-    <li v-for="country in countries">{{ country.name.official }}</li>
-  </ol>
+  <button
+    type="button"
+    class="m-2 btn"
+    @click="toggleButton"
+    :class="{
+      'btn-primary': isPrimary,
+      'btn-danger': isDanger,
+    }"
+  >
+    MI LISTA
+  </button>
 </template>
